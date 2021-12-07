763f0ab5e03b7b7556c8a89a20243bbc6332ff88
William Iheanacho <williami@vandenrobotics.com>
2021-12-07 15:50:36 -0800

    removed files

--- app/src/main/java/com/example/strongholdtestapp/ui/HomeFragment.java
+++ app/src/main/java/com/example/strongholdtestapp/ui/HomeFragment.java
@@ -1,4 +1,4 @@
-package com.example.strongholdtestapp.ui.home;
+package com.example.strongholdtestapp.ui;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,28 +13,18 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.strongholdtestapp.R;
-import com.example.strongholdtestapp.databinding.FragmentHomeBinding;
+import com.example.strongholdtestapp.databinding.FragmentEventsBinding;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
-    private FragmentHomeBinding binding;
+    private FragmentEventsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentEventsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 
